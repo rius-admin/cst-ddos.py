0c5bfe0dec35920e21df9da73d2f92e173587761
rius-admin <158822649+rius-admin@users.noreply.github.com>
2024-04-12 16:13:12 +0700

    Update cst.py

--- cst.py
+++ cst.py
@@ -30,7 +30,7 @@ port = input("Port   : ")
 
 os.system("clear")
 os.system("figlet Attack")
-print "[                    ] 0% "
+print "[=>                  ] 0% "
 time.sleep(5)
 print "[===>                ] 25%"
 time.sleep(5)
